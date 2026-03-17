d9041f35b85f2142e28dbd98c7dce20a621c8c77
Julian Cruz <julianchozen@gmail.com>
2026-03-17 14:40:27 -0500

    actualiacion5

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -7,38 +7,35 @@
 <div class="grid-productos">
 
 <div class="producto">
-<img src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b" alt="Camiseta">
+<img src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b">
 <h3>Camiseta Negra</h3>
 <p class="precio">$50.000</p>
 
-<button onclick="agregarCarrito('Camiseta Negra',50000)" class="boton-agregar">
+<button onclick="agregarCarrito('Camiseta Negra',50000,'https://images.unsplash.com/photo-1523381210434-271e8be1f52b')" class="boton-agregar">
 <i class="fa-solid fa-cart-plus"></i> Agregar
 </button>
-
 </div>
 
 
 <div class="producto">
-<img src="https://images.unsplash.com/photo-1520975922071-a6a5f1e6c9d0" alt="Hoodie">
+<img src="https://images.unsplash.com/photo-1520975922071-a6a5f1e6c9d0">
 <h3>Hoodie Blanca</h3>
 <p class="precio">$90.000</p>
 
-<button onclick="agregarCarrito('Hoodie Blanca',90000)" class="boton-agregar">
+<button onclick="agregarCarrito('Hoodie Blanca',90000,'https://images.unsplash.com/photo-1520975922071-a6a5f1e6c9d0')" class="boton-agregar">
 <i class="fa-solid fa-cart-plus"></i> Agregar
 </button>
-
 </div>
 
 
 <div class="producto">
-<img src="https://images.unsplash.com/photo-1520975698519-59a5c6b0c0e1" alt="Gorra">
+<img src="https://images.unsplash.com/photo-1520975698519-59a5c6b0c0e1">
 <h3>Gorra Urbana</h3>
 <p class="precio">$35.000</p>
 
-<button onclick="agregarCarrito('Gorra Urbana',35000)" class="boton-agregar">
+<button onclick="agregarCarrito('Gorra Urbana',35000,'https://images.unsplash.com/photo-1520975698519-59a5c6b0c0e1')" class="boton-agregar">
 <i class="fa-solid fa-cart-plus"></i> Agregar
 </button>
-
 </div>
 
 </div>
